a39e142985f9b842d2428371fed7fcb071f47464
kirill-shevchuk17 <kirill-shevchuk1@mail.ru>
2021-05-18 15:46:39 +0300

    Create views and urls for him

--- src/trello/urls.py
+++ src/trello/urls.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from .views import *
+
 
 urlpatterns = [
+	path('project/detail/', ProjectDetailView.as_view()),
+	path('project/create/', ProjectCreateView.as_view()),
+	path('project/all/', ProjectListSerializer.as_view())
 ]
